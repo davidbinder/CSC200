b0697c2ea1b563bd2f7086e8667994869fd77193
davidbinder <davidbinder360@gmail.com>
2014-09-24 11:38:27 -0400

    assignment 2 update

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class Assignment2{
-
+/*
+Program Flowchart
+1. make scanner
+2. make int sum, temp, average
+3. loop
+4. sum = sum + next integer
+5. exit loop when ammofnumbs = i
+6. average = sum/ammofnumbs
+7. print results
+*/
 	public static void main(String[] args){
 		//ammount of numbers to enter
 		int ammofnumbs = 5;
 		Scanner kbd = new Scanner(System.in);
-		int[] numbs = new int[(ammofnumbs)];
 		System.out.println("Enter " + ammofnumbs + " numbers.");
 		int sum = 0;
 		int temp = 0;
 		for(int i = 0; i < ammofnumbs; i++){
 			temp = kbd.nextInt();
-			numbs[i] = temp;
 			sum += temp;
 		}
 		int average = sum/ammofnumbs;
